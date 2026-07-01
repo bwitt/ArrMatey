0daa1340b4980a77b79f9eac1ae1e3be6b17401b
Owen LeJeune <omlejeune@gmail.com>
2026-07-01 16:50:11 -0400

    uprev apps

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -84,8 +84,8 @@ android {
         applicationId = "com.dnfapps.arrmatey"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 23
-        versionName = "0.6.0"
+        versionCode = 24
+        versionName = "0.6.1"
     }
     packaging {
         resources {
